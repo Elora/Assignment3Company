6d39aecad44d00cb7f93a3afe2bedf7c5f69f878
0nenote <soufiane.s@gmail.com>
2014-12-09 16:44:40 +0100

    v1.1

--- CompanyX/src/kb50/companyx/CompanyList.java
+++ CompanyX/src/kb50/companyx/CompanyList.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.ListActivity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,12 +15,13 @@ import android.widget.Toast;
 
 public class CompanyList extends ListActivity {
 
-	String[] companies = new String[200];
 	List<Company> companyList;
+	String[] companies; 
+	
 	public void stringToArr() {
 
 		companyList = getAllCompanies();
-
+		companies = new String[companyList.size()];
 		for (int i = 0; i < companyList.size(); i++) {
 
 			companies[i] = getAllCompanies().get(i).getName();
@@ -56,12 +58,24 @@ public class CompanyList extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState); // ---no need to call this---
+		Company c = new Company();
+		c.setInfo("some commpany info");
+		c.setName("Google inc");
+		c.setSite("Google.com");
 		
+		
+		ContentValues companyValues = new ContentValues();
+		companyValues.put("name", c.getName());
+		companyValues.put("website", c.getSite());
+		companyValues.put("info", c.getInfo());
+		
+		getContentResolver().insert(Uri.parse("content://com.example.appcontentprovider.CompanyProvider/company"), companyValues);
 		stringToArr();// //setContentView(R.layout.main);
 		setListAdapter(new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, companies));
 	}
 
+	@Override
 	public void onListItemClick(ListView parent, View v, int position, long id) {
 		Toast.makeText(this, "You have selected " + companies[position],
 				Toast.LENGTH_SHORT).show();
